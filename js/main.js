9c63e9fb6fdb3d9cda7ea0ffc4fff3799e7c9131
cantwell <rjazantsev@gmail.com>
2020-05-05 19:47:39 +0300

    open/close modal windows

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
+const modalCart = document.querySelector(".modal-cart");
 const close = document.querySelector(".close");
 
 cartButton.addEventListener("click", toggleModal);
 close.addEventListener("click", toggleModal);
 
 function toggleModal() {
-  modal.classList.toggle("is-open");
+  modalCart.classList.toggle("is-open");
 }
+
+//day 1
+const buttonAuth = document.querySelector('.button-auth');
+const buttonOut = document.querySelector('.button-out');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const buttonLogin = document.querySelector('.button-login');
+
+let login = '';
+
+const toggleModalAuth = () => {
+  modalAuth.classList.toggle('is-open');
+}
+
+buttonAuth.addEventListener('click', toggleModalAuth);
+closeAuth.addEventListener('click', toggleModalAuth);
